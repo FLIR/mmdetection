0b9be6e13fc863fb98d211b24aae119cd5dc3e00
Zachary Tane <zachary.tane@flir.com>
2020-09-04 09:47:53 -0700

    added expected labelvector for ground tanks

--- configs/_base_/datasets/custom_flir_instance.py
+++ configs/_base_/datasets/custom_flir_instance.py
@@ -27,8 +27,9 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
-custom_classes = ('person', 'bicycle', 'vehicle', 'motorcycle', 'airplane', 'bus',
-               'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+# custom classes for rodeo ground
+custom_classes = ('person', 'bicycle', 'vehicle', 'motorcycle', 'fixed wing', 'military_person',
+               'train', 'military_vehicle', 'boat', 'traffic light', 'fire hydrant',
                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
@@ -40,7 +41,24 @@ custom_classes = ('person', 'bicycle', 'vehicle', 'motorcycle', 'airplane', 'bus
                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
                'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
-               'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+               'vase', 'scissors', 'teddy bear', 'phantom', 'toothbrush')
+
+
+# custom classes for test data
+# custom_classes = ('person', 'bicycle', 'vehicle', 'motorcycle', 'airplane', 'bus',
+#                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+#                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+#                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+#                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+#                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+#                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+#                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+#                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+#                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+#                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+#                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+#                'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
+#                'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
 # for original <200 img ATR dataset
 data = dict(
     samples_per_gpu=2,
